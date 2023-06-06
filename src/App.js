725fa603f0db18307bf39113087fda9b09186176
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-06-06 17:24:33 +0530

    horizontal drag drop working fine

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import DragDrop from "./test_components/DragDrop";
 import LazyLoadLab from "./test_components/LazyLoadLab";
 import TrelloTest from "./test_components/TrelloTest";
 import Trello from "./test_components/Trello";
+import HorDragDrop from "./test_components/HorDragDrop";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       {/* <ReactBeautifulDND /> */}
       {/* <LazyLoadLab /> */}
       {/* <TrelloTest /> */}
-      <Trello />
+      {/* <Trello /> */}
+      <HorDragDrop />
     </div>
   );
 }
